d5b6a8ee97c1074319e1332a8a4fe7c9f89d460d
loyolastalin <loyolastalin@gmail.com>
2021-01-21 13:55:22 +0530

    -

---
+++ MessageBroker/RabbitMqPublisher/Publisher.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    public static class Publisher
+    {
+        private static string URL = $"amqps://ptbgyuvf:wjdH3fGw5c-IcRJYfucl3yDRH-bb9hfr@fox.rmq.cloudamqp.com/ptbgyuvf";
+        private static string EXCHANGE = "delivered-message";
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(URL),                
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = false;
+                var propertiesDictionary = new Dictionary<string, object>();
+                properties.Headers = propertiesDictionary;
+                propertiesDictionary.Add("app_id", "Matlus Video site");
+                propertiesDictionary.Add("content_type", "text/plain");
+
+                do
+                {
+                    propertiesDictionary["message_id"] = Guid.NewGuid().ToString("N");
+                    var messageBody = GetMessageBody();
+                    var body = Encoding.UTF8.GetBytes(messageBody);
+                    channel.BasicPublish(exchange: EXCHANGE, routingKey: string.Empty, properties, body: body);
+                    Console.WriteLine($"Sent Message: {messageBody}");
+                    Console.ReadLine();
+                    
+                }
+                while (true);
+            }
+        }
+
+        private static string GetMessageBody()
+        {
+            return $"date_time: {DateTime.UtcNow.ToString()}" +
+                $"\r\ndata: This is the body of the message";
+        }
+    }
+}
---
+++ MessageBroker/RabbitMqSubscriber/Subscriber.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqSubscriber
+{
+    public static class Subscriber
+    {
+        private static string URL = $"amqps://ptbgyuvf:wjdH3fGw5c-IcRJYfucl3yDRH-bb9hfr@fox.rmq.cloudamqp.com/ptbgyuvf";
+        private static string QUEUENAME = "delevered-queue";
+        static void Main(string[] args)
+        {
+
+            var connectionFactory = new ConnectionFactory()
+            {
+                Uri = new Uri(URL),
+            };
+
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine("Waiting for Messages...");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var appId = Encoding.UTF8.GetString((byte[])ea.BasicProperties.Headers["app_id"]);
+                    var messageId = Encoding.UTF8.GetString((byte[])ea.BasicProperties.Headers["message_id"]);
+                    var contentType = Encoding.UTF8.GetString((byte[])ea.BasicProperties.Headers["content_type"]);
+                    Console.WriteLine($"App Id: {appId}\tMessage Id: {messageId}\tContent Type: {contentType}");
+
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine($"[x] {message}");
+                    channel.BasicAck(ea.DeliveryTag, multiple: false);
+                };
+
+
+                var consumerTag = channel.BasicConsume(queue: QUEUENAME, autoAck: false, consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                channel.BasicCancel(consumerTag);
+            }
+        }
+    }
+}
